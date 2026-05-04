2c8eb18308d59aa90a2f2aeca3a194c536236064
rsatnarain <type.unique_0o@icloud.com>
2026-05-04 19:27:22 -0400

    Feat: Update load testing parameters for increased user simulation

--- load_test.js
+++ load_test.js
@@ -3,9 +3,9 @@ import { sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '30s', target: 100 }, // Ramp up to 100 users over 30 seconds
-    { duration: '1m', target: 500 },  // Spike to 500 users and hold for 1 minute (The 9 AM Herd)
-    { duration: '30s', target: 0 },   // Ramp down to 0
+    { duration: '15s', target: 2100 }, // Ramp up to 2100 users over 15 seconds
+    { duration: '1m', target: 15000 },  // Spike to 15000 users and hold for 1 minute (The 9 AM Herd)
+    { duration: '15s', target: 0 },   // Ramp down to 0
   ],
 };
 
